19d3bdedb73f976874c1b11ee89e5cf82909bc0c
AdelManseur <153110085+AdelManseur@users.noreply.github.com>
2024-01-15 13:36:18 +0100

    Update and rename maktaba.js to maktaba-v1.js

---
+++ maktaba-v1.js
@@ -0,0 +1,20 @@
+ var myMathLibrary = (function () {
+      // Private functions
+      function square(x) {
+        return x * x;
+      }
+
+      function squareRoot(x) {
+        return Math.sqrt(x);
+      }
+
+      // Public API (an object)
+      return {
+        calculateSquare: function (number) {
+          return square(number);
+        },
+        calculateSquareRoot: function (number) {
+          return squareRoot(number);
+        }
+      };
+    })();
