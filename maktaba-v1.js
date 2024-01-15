c136446c932d6e270c43eb901b9369dadb2a183c
AdelManseur <153110085+AdelManseur@users.noreply.github.com>
2024-01-15 12:04:48 +0100

    Create maktaba-v1.js

---
+++ maktaba-v1.js
@@ -0,0 +1,13 @@
+var myMathLibrary = (function () {
+  // Private function
+  function square(x) {
+    return x * x;
+  }
+
+  // Public API (an object)
+  return {
+    calculateSquare: function (number) {
+      return square(number);
+    }
+  };
+})();
