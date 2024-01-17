8e125f3351167b90d669316c8e2c7887d8e3c370
AdelManseur <153110085+AdelManseur@users.noreply.github.com>
2024-01-17 15:21:38 +0100

    Update and rename maktabaLibrary-v21.js to maktabaLibrary-v22.js

--- maktabaLibrary-v22.js
+++ maktabaLibrary-v22.js
@@ -35,7 +35,7 @@ var maktaba = (function() {
     if (typeof innerHTML === 'undefined' || typeof innerHTML !== 'string') {
       throw new Error('innerHTML must be a string');
     }
-    if (typeof iscreated === 'undefined' || isCreated !== 'string') {
+    if (typeof iscreated === 'undefined' || isCreated !== 'true' && isCreated !== 'false') {
       throw new Error('isCreated must be a string');
     }
   }
